abd6f66cd6648750fcc5e300a8ff4bbea3b1bc20
BiBee08 <114662774+BiBee08@users.noreply.github.com>
2022-12-06 12:27:11 -0300

    Atualização 06/12/2022
    Quase finalizado, mexemos com o css e passamos tudo para o banco

--- src/Models/Livros.js
+++ src/Models/Livros.js
@@ -6,11 +6,25 @@ const LivrosSchema = new Schema({
     link: String,
     title: String,
     //------------------//
+    linkCompra: String,
+    linkCompra1: String,
+    linkCompra2: String,
+
     nome: String,
     autor: String,
     qtdPaginas: String,
     dataLancamento: String,
-
+    imgLivro: String,
+    nome1: String,
+    autor1: String,
+    qtdPaginas1: String,
+    dataLancamento1: String,
+    imgLivro1: String,
+    nome2: String,
+    autor2: String,
+    qtdPaginas2: String,
+    dataLancamento2: String,
+    imgLivro2: String
 })
 
 module.exports = model('livros', LivrosSchema)
--- src/database/db.js
+++ src/database/db.js
@@ -1,3 +1,4 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/web').then(e => console.log("Conected in MongoDB")).catch(e => console.log(e))
+mongoose.connect('mongodb://localhost:27017/web').then(e => console.log("Conected in MongoDB")).catch(e => console.log(e))
+//127.0.0.1
